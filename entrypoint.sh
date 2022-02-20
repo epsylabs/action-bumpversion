92bfa8b2103ce162f160e73d7d5b16206e8c375e
Michal Przytulski <michal@przytulski.pl>
2022-02-20 13:42:03 +0100

    add input and fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 part=${DEFAULT_BUMP:-patch}
-source=${SOURCE:-.}
-dryrun=${DRY_RUN:-false}
-new_version=${NEW_VERSION:-""}
 
-cd "${GITHUB_WORKSPACE}/${source}" || return
 
+/app/bin/bump2version "$part" --verbose
+
+raw_output=$(bumpversion --list "$part" --dry-run)
 
 old_version=$(echo "$raw_output" | grep -o 'current_version=\S*' | cut -d= -f2)
 new_version=$(echo "$raw_output" | grep -o 'new_version=\S*' | cut -d= -f2)
@@ -18,10 +17,3 @@ echo ::set-output name=part::"$part"
 echo "Semantic Version Part to Bump: $part"
 echo "Current Version: $old_version"
 echo "New Version: $new_version"
-
-if [ "$dryrun" = true ]; then
-  echo "Dryrun is set to true. Exiting..."
-  exit 0
-else
-  /app/bin/bump2version "$part" --new-version="$new_version" --verbose
-fi
