d71d25ad74ca9fc496087be1ad9c18a9023acd76
Michal Przytulski <michal@przytulski.pl>
2022-02-23 13:33:35 +0100

    fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ set -ex
 part=${DEFAULT_BUMP:-patch}
 
 
-/app/bin/bump2version "$part" --verbose
-
 raw_output=$(/app/bin/bump2version --list "$part" --dry-run)
 
 old_version=$(echo "$raw_output" | grep -o 'current_version=\S*' | cut -d= -f2)
 new_version=$(echo "$raw_output" | grep -o 'new_version=\S*' | cut -d= -f2)
+
+/app/bin/bump2version "$part" --verbose
 
 echo ::set-output name=old_ver::"$old_version"
 echo ::set-output name=new_ver::"$new_version"
